6bea511c3039bcd9fdc9457f166591f0980db5f1
kai <thiskaiguy@gmail.com>
2020-07-24 11:12:21 +0100

    Store parker and waker in thread local storage

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,29 @@ use {
     crossbeam::sync::Parker,
     std::{
         future::Future,
-        task::{Context, Poll},
+        task::{Context, Poll, Waker},
     },
 };
 
 pub fn block_on<F: Future>(future: F) -> F::Output {
     pin_utils::pin_mut!(future);
 
-    let parker = Parker::new();
-    let unparker = parker.unparker().clone();
-    let waker = async_task::waker_fn(move || unparker.unpark());
-
-    let cx = &mut Context::from_waker(&waker);
-    loop {
-        match future.as_mut().poll(cx) {
-            Poll::Ready(output) => return output,
-            Poll::Pending => parker.park(),
-        }
+    thread_local! {
+        static CACHE: (Parker, Waker) = {
+            let parker = Parker::new();
+            let unparker = parker.unparker().clone();
+            let waker = async_task::waker_fn(move || unparker.unpark());
+            (parker, waker)
+        };
     }
+
+    CACHE.with(|(parker, waker)| {
+        let cx = &mut Context::from_waker(&waker);
+        loop {
+            match future.as_mut().poll(cx) {
+                Poll::Ready(output) => return output,
+                Poll::Pending => parker.park(),
+            }
+        }
+    })
 }
