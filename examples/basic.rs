4135f5543730fe61f1bad8ab20292e8047276288
kai <thiskaiguy@gmail.com>
2020-07-24 12:13:30 +0100

    Create a spawn function

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,8 +1,17 @@
-use {executor::block_on, futures_timer::Delay, std::time::Duration};
+use {executor::{block_on, spawn}, futures_timer::Delay, std::time::Duration};
 
 pub fn main() {
     block_on(async {
-        Delay::new(Duration::from_secs(1)).await;
-        println!("delay");
+        let handle = spawn(async {
+            seconds(5).await;
+        });
+        seconds(1).await;
+        println!("delay1");
+        handle.await;
+        println!("delay2");
     });
+}
+
+fn seconds(seconds: u64) -> Delay {
+    Delay::new(Duration::from_secs(seconds))
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,19 @@
 use {
-    crossbeam::sync::Parker,
+    crossbeam::{channel, sync::Parker},
+    futures::channel::oneshot,
+    once_cell::sync::Lazy,
     std::{
         cell::RefCell,
         future::Future,
+        pin::Pin,
+        sync::{atomic::{AtomicUsize, Ordering}, Arc, Mutex},
         task::{Context, Poll, Waker},
+        thread,
     },
 };
 
 pub fn block_on<F: Future>(future: F) -> F::Output {
-    pin_utils::pin_mut!(future);
+    futures::pin_mut!(future);
 
     thread_local! {
         static CACHE: RefCell<(Parker, Waker)> = {
@@ -30,3 +35,71 @@ pub fn block_on<F: Future>(future: F) -> F::Output {
         }
     })
 }
+
+type JoinHandle<R> = Pin<Box<dyn Future<Output = R> + Send>>;
+
+pub fn spawn<F, R>(future: F) -> JoinHandle<R>
+where
+    F: Future<Output = R> + Send + 'static,
+    R: Send + 'static,
+{
+    let (s, r) = oneshot::channel();
+    let future = async move {
+        let _ = s.send(future.await);
+    };
+
+    let task = Arc::new(Task {
+        state: AtomicUsize::new(0),
+        future: Mutex::new(Box::pin(future)),
+    });
+
+    QUEUE.send(task).unwrap();
+
+    Box::pin(async { r.await.unwrap() })
+}
+
+
+const WOKEN: usize = 0b01;
+const RUNNING: usize = 0b10;
+
+struct Task {
+    state: AtomicUsize,
+    future: Mutex<Pin<Box<dyn Future<Output = ()> + Send>>>,
+}
+impl Task {
+    pub fn run(self: Arc<Self>) {
+        let task = self.clone();
+        let waker = async_task::waker_fn(move || {
+            // schedule if the task is not woken already and is not running
+            if task.state.fetch_or(WOKEN, Ordering::SeqCst) == 0 {
+                QUEUE.send(task.clone()).unwrap();
+            }
+        });
+
+        self.state.store(RUNNING, Ordering::SeqCst);
+        let cx = &mut Context::from_waker(&waker);
+        let poll = self.future.try_lock().unwrap().as_mut().poll(cx);
+
+        // schedule if the task was woken while running
+        if poll.is_pending() {
+            if self.state.fetch_and(!RUNNING, Ordering::SeqCst) == WOKEN | RUNNING {
+                QUEUE.send(self).unwrap();
+            }
+        }
+    }
+}
+
+static QUEUE: Lazy<channel::Sender<Arc<Task>>> = Lazy::new(|| {
+    let (sender, receiver) = channel::unbounded::<Arc<Task>>();
+
+    for _ in 0..num_cpus::get().max(1) {
+        let receiver = receiver.clone();
+        thread::spawn(move || {
+            for task in receiver.iter() {
+                task.run();
+            }
+        });
+    }
+
+    sender
+});
