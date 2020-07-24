742842939c53f5527402e99bd953fc6119bf73cf
kai <thiskaiguy@gmail.com>
2020-07-24 11:09:56 +0100

    Park threads with crossbeam

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,23 @@
-use std::{future::Future, task::{Context, Poll}, thread};
+use {
+    crossbeam::sync::Parker,
+    std::{
+        future::Future,
+        task::{Context, Poll},
+    },
+};
 
 pub fn block_on<F: Future>(future: F) -> F::Output {
     pin_utils::pin_mut!(future);
 
-    let thread = thread::current();
-    let waker = async_task::waker_fn(move || thread.unpark());
+    let parker = Parker::new();
+    let unparker = parker.unparker().clone();
+    let waker = async_task::waker_fn(move || unparker.unpark());
 
     let cx = &mut Context::from_waker(&waker);
     loop {
         match future.as_mut().poll(cx) {
             Poll::Ready(output) => return output,
-            Poll::Pending => thread::park(),
+            Poll::Pending => parker.park(),
         }
     }
 }
