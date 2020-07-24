45a33a3d156ab5adbd508811ead341803dd77be2
kai <thiskaiguy@gmail.com>
2020-07-24 11:14:54 +0100

    Forbid recursive block_on

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use {
     crossbeam::sync::Parker,
     std::{
+        cell::RefCell,
         future::Future,
         task::{Context, Poll, Waker},
     },
@@ -10,15 +11,16 @@ pub fn block_on<F: Future>(future: F) -> F::Output {
     pin_utils::pin_mut!(future);
 
     thread_local! {
-        static CACHE: (Parker, Waker) = {
+        static CACHE: RefCell<(Parker, Waker)> = {
             let parker = Parker::new();
             let unparker = parker.unparker().clone();
             let waker = async_task::waker_fn(move || unparker.unpark());
-            (parker, waker)
+            RefCell::new((parker, waker))
         };
     }
 
-    CACHE.with(|(parker, waker)| {
+    CACHE.with(|cache| {
+        let (parker, waker) = &mut *cache.try_borrow_mut().expect("recursive `block_on`");
         let cx = &mut Context::from_waker(&waker);
         loop {
             match future.as_mut().poll(cx) {
